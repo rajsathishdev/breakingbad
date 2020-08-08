dbf8f06447b6650afc182b45dfed4e940ffc6478
sathish <sathish@chennovate.com>
2020-08-08 16:43:38 +0530

    practice react

---
+++ src/components/ui/characters/CharacterGrid.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import CharacterItem from './CharacterItem';
+import Spinner from '../Spinner';
+
+const CharacterGrid = ({items, isLoading}) => {
+    
+        return (isLoading)? (
+            <Spinner />
+        ) : (
+            <section className="cards">
+                {
+                    items.map((item) => (
+                        <CharacterItem key="{item.char_id }" item={ item } />
+                    ))
+                }
+            </section>
+        );
+    
+}
+
+export default CharacterGrid;
